a2eb2bf083aa80a57474639dd7a7150158466bb2
Valerii <amabatman.12@gmail.com>
2024-10-01 18:29:03 +0600

    Add comments to code

--- Hero.py
+++ Hero.py
@@ -1,6 +1,7 @@
 class SuperHero:
     people = 'people'
 
+    #Инициализация
     def __init__(self, name, nickname, superpower, health_point, catchphrase):
         self.name = name
         self.nickname = nickname
@@ -8,21 +9,27 @@ class SuperHero:
         self.health_point = health_point
         self.catchphrase = catchphrase
 
+    #Метод вывода имени супергероя
     def super_name(self):
         return self.name
 
+    #Метод увеличения здоровья героя в два раза
     def double_HP(self):
         self.health_point *= 2
         return self.health_point
 
+    #Магический метод, который выводит прозвище героя, его суперспособность и здоровье
     def __str__(self):
         return f"Nickname: {self.nickname}, Superpower: {self.superpower}, Health: {self.health_point}"
 
+    #Магический метод расчета длины коронной фразы героя
     def __len__(self):
         return len(self.catchphrase)
 
+#Создание экземпляра
 new_hero = SuperHero('Batman', 'Batty', 'Money', 147, 'I`m Batman!')
 
+#Вызов всех методов
 print(new_hero.super_name())
 print(new_hero.double_HP())
 print(new_hero)
