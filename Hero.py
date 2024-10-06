435d76d31d3f09f25257545e42081d9ed82a83fd
Valerii <amabatman.12@gmail.com>
2024-10-06 17:27:08 +0600

    Add new HP method and phrase method

--- Hero.py
+++ Hero.py
@@ -1,7 +1,7 @@
 class SuperHero:
     people = 'people'
 
-    #Инициализация
+    # Инициализация
     def __init__(self, name, nickname, superpower, health_point, catchphrase, damage):
         self.name = name
         self.nickname = nickname
@@ -9,72 +9,62 @@ class SuperHero:
         self.health_point = health_point
         self.catchphrase = catchphrase
         self.damage = damage
-        self.fly = False
+        self.fly = False  # Добавляем свойство fly по умолчанию
 
-    #Метод вывода имени супергероя
+    # Метод вывода имени супергероя
     def super_name(self):
         return self.name
 
-    #Метод увеличения здоровья героя в два раза
-    def double_HP(self):
-        self.health_point *= 2
+    # Метод возведения здоровья героя в квадрат и изменение fly на True
+    def square_HP(self):
+        self.health_point **= 2  # Возводим здоровье в квадрат
+        self.fly = True  # Меняем значение fly на True
         return self.health_point
 
-    #Магический метод, который выводит прозвище героя, его суперспособность и здоровье
-    def __str__(self):
-        return f"Nickname: {self.nickname}, Superpower: {self.superpower}, Health: {self.health_point}, Damage: {self.damage}"
+    # Новый метод, выводящий фразу
+    def true_phrase(self):
+        return "True in the True_phrase"
 
-    #Магический метод расчета длины коронной фразы героя
+    # Магический метод, который выводит прозвище героя, его суперспособность и здоровье
+    def __str__(self):
+        return f"Nickname: {self.nickname}, Superpower: {self.superpower}, Health: {self.health_point}, Damage: {self.damage}, Fly: {self.fly}"
+
+    # Магический метод расчета длины коронной фразы героя
     def __len__(self):
         return len(self.catchphrase)
+
 
 # Класс огненного героя (наследуется от SuperHero)
 class Fire(SuperHero):
     def __init__(self, name, nickname, superpower, health_point, catchphrase, damage):
         super().__init__(name, nickname, superpower, health_point, catchphrase, damage)
-        self.fly = True  # Огненный герой может летать
 
-    # Переопределение метода увеличения здоровья
-    def double_HP(self):
-        self.health_point *= 3  # Увеличивает здоровье в 3 раза вместо 2
-        return self.health_point
-
-    # Переопределение магического метода
+    # Переопределение магического метода __str__
     def __str__(self):
         return f"Fire Hero - {super().__str__()}"
 
+# Класс водного героя (наследуется от SuperHero)
 class Water(SuperHero):
     def __init__(self, name, nickname, superpower, health_point, catchphrase, damage):
         super().__init__(name, nickname, superpower, health_point, catchphrase, damage)
-        self.fly = False
 
-    def double_HP(self):
-        self.health_point += 1.5
-        return self.health_point
-
+    # Переопределение магического метода __str__
     def __str__(self):
-        return f"Water hero - {super().__str__()}"
+        return f"Water Hero - {super().__str__()}"
 
-
-#Создание экземпляра
-new_hero = SuperHero('Batman', 'Batty', 'Money', 147, 'I`m Batman!')
 # Создание экземпляров героев
 fire_hero = Fire('FireMan', 'Blaze', 'Fire Manipulation', 100, 'Burn them all!', 50)
 water_hero = Water('WaterMan', 'Splash', 'Water Control', 120, 'Feel the wave!', 40)
 
-#Вызов всех методов
-print(new_hero.super_name())
-print(new_hero.double_HP())
-print(new_hero)
-print(len(new_hero))
-
-# Вызов всех методов для каждого героя
+# Вызов методов для каждого героя
 print(fire_hero.super_name())
-print(fire_hero.double_HP())
+print(fire_hero.square_HP())  # Возведение здоровья в квадрат и изменение fly
 print(fire_hero)
+print(fire_hero.true_phrase())  # Вызов метода с фразой
 print(len(fire_hero))
 
 print(water_hero.super_name())
-print(water_hero.double_HP())
+print(water_hero.square_HP())  # Возведение здоровья в квадрат и изменение fly
 print(water_hero)
+print(water_hero.true_phrase())  # Вызов метода с фразой
 print(len(water_hero))
