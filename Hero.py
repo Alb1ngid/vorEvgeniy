6206b855f001c3125c4656a2f8370ac9a26ba7ea
Valerii <amabatman.12@gmail.com>
2024-10-06 17:25:48 +0600

    Add waterHero class

--- Hero.py
+++ Hero.py
@@ -2,12 +2,14 @@ class SuperHero:
     people = 'people'
 
     #Инициализация
-    def __init__(self, name, nickname, superpower, health_point, catchphrase):
+    def __init__(self, name, nickname, superpower, health_point, catchphrase, damage):
         self.name = name
         self.nickname = nickname
         self.superpower = superpower
         self.health_point = health_point
         self.catchphrase = catchphrase
+        self.damage = damage
+        self.fly = False
 
     #Метод вывода имени супергероя
     def super_name(self):
@@ -20,7 +22,7 @@ class SuperHero:
 
     #Магический метод, который выводит прозвище героя, его суперспособность и здоровье
     def __str__(self):
-        return f"Nickname: {self.nickname}, Superpower: {self.superpower}, Health: {self.health_point}"
+        return f"Nickname: {self.nickname}, Superpower: {self.superpower}, Health: {self.health_point}, Damage: {self.damage}"
 
     #Магический метод расчета длины коронной фразы героя
     def __len__(self):
@@ -37,16 +39,42 @@ class Fire(SuperHero):
         self.health_point *= 3  # Увеличивает здоровье в 3 раза вместо 2
         return self.health_point
 
-    # Переопределение магического метода __str__
+    # Переопределение магического метода
     def __str__(self):
         return f"Fire Hero - {super().__str__()}"
+
+class Water(SuperHero):
+    def __init__(self, name, nickname, superpower, health_point, catchphrase, damage):
+        super().__init__(name, nickname, superpower, health_point, catchphrase, damage)
+        self.fly = False
+
+    def double_HP(self):
+        self.health_point += 1.5
+        return self.health_point
+
+    def __str__(self):
+        return f"Water hero - {super().__str__()}"
 
 
 #Создание экземпляра
 new_hero = SuperHero('Batman', 'Batty', 'Money', 147, 'I`m Batman!')
+# Создание экземпляров героев
+fire_hero = Fire('FireMan', 'Blaze', 'Fire Manipulation', 100, 'Burn them all!', 50)
+water_hero = Water('WaterMan', 'Splash', 'Water Control', 120, 'Feel the wave!', 40)
 
 #Вызов всех методов
 print(new_hero.super_name())
 print(new_hero.double_HP())
 print(new_hero)
 print(len(new_hero))
+
+# Вызов всех методов для каждого героя
+print(fire_hero.super_name())
+print(fire_hero.double_HP())
+print(fire_hero)
+print(len(fire_hero))
+
+print(water_hero.super_name())
+print(water_hero.double_HP())
+print(water_hero)
+print(len(water_hero))
